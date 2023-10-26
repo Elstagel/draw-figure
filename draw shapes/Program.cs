4752d124d8dadbff603e4c407442d3f4f77444a5
Станислав Губарь <Elstagel@gmail.com>
2023-10-27 00:28:51 +0300

    шш..

--- draw shapes/Program.cs
+++ draw shapes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -161,9 +162,122 @@ namespace draw_shapes
 			show_figure(array, size);
 		}
 
+		static void seventh_qu(int size)
+		{
+			size= size * 2;
+			char[,] array = new char[size / 2, size];
+
+			for (int i = 0; i < size / 2; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (i == 0 && j == 0) array[i, j]= '┌';
+					else if(i == 0 && j == size - 1) array[i, j]= '┐';
+					else if(i == size/2-1 && j == 0) array[i, j]= '└';
+					else if(i == size/2-1 && j == size - 1) array[i, j]= '┘';
+					else if(i == 0 || i == size/2-1) array[i, j] = '─';
+					else if (j == 0 || j == size-1) array[i, j] = '│';
+					else if ((i + j) % 2 == 0){array[i, j] = '8';}
+					else if ((i + j) % 2 == 1){array[i, j] = '4';}
+
+
+				}
+			}
+
+			for (int i = 0; i < size / 2; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (array[i, j] == '8')
+					{
+						Console.BackgroundColor = ConsoleColor.White;
+						Console.ForegroundColor = ConsoleColor.White;
+						Console.Write(array[i, j]);
+						color_end();
+
+					}
+					else if (array[i, j] == '4')
+					{
+						Console.BackgroundColor = ConsoleColor.Black;
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.Write(array[i, j]);
+						color_end();
+
+					}
+					else Console.Write(array[i, j]); 
+				}
+				Console.WriteLine();
+			}
+		}
+
+		static void eighth_qu(int size)							 
+		{
+			size= size / 2;
+			char[,,,] array = new char[size, size, size, size];
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					for (int k = 0; k < size; k++)
+					{
+						for (int l = 0; l < size; l++)
+						{
+							if ((i + k) % 2 == 0)
+							{
+								array[i, j, k, l] = '*';
+							}
+							else
+							{
+								array[i, j, k, l] = '-';
+							}
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					for (int k = 0; k < size; k++)
+					{
+						for (int l = 0; l < size; l++)
+						{
+							if ((i + k) % 2 == 0)
+							{
+								Console.Write(array[i, j, k, l] + " ");
+							}
+							else
+							{
+							    Console.Write(array[i, j, k, l] + " ");	
+							}
+						}
+					}
+					Console.WriteLine();
+				}
+			}
+		}
+
 		static void color_start(int number_color)
 		{
-
+			//Black       0
+			//Blue        9
+			//Cyan        11
+			//DarkBlue    1
+			//DarkCyan    3
+			//DarkGray    8
+			//DarkGreen   2
+			//DarkMagenta 5
+			//DarkRed     4
+			//DarkYellow  6
+			//Gray        7
+			//Green       10
+			//Magenta     13
+			//Red     12
+			//White       15
+			//Yellow      14
+			
 			ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = colors[number_color];
@@ -180,8 +294,10 @@ namespace draw_shapes
 		static void Main(string[] args)
 		{
 			int size = 5;
-
 			Console.WriteLine("Введите размер фигур в диапозоне от 1-20:");
+			color_start(14);
+			Console.WriteLine("Для 7 и 8 фигуры размер уменьшен для коректной отрисовки всех фигур");
+			color_end();
 			size = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine();
 
@@ -255,6 +371,19 @@ namespace draw_shapes
 			color_end();
 			sixth_qu(size);
 			Console.WriteLine();
+
+			color_start(13);
+			Console.WriteLine("7-фигура");
+			color_end();
+			seventh_qu(size);
+			Console.WriteLine();
+
+			color_start(13);
+			Console.WriteLine("8-фигура(размер уменьшен в двое)");
+			color_end();
+			eighth_qu(size);
+			Console.WriteLine();
+
 		}
 	}
 }
